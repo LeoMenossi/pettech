5f38d05ab2f86cdbc24d1ba1f47f7f12bdf14d65
leonardo.menossi <leonardo.menossi@cixbrasil.com>
2025-01-15 22:18:37 -0300

    subindo alterações

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import "reflect-metadata"
+import '@/lib/typeorm/typeorm'
 import fastify from 'fastify'
 import { personRoutes } from '@/http/controllers/person/routes'
 import { userRoutes } from './http/controllers/user/routes'
@@ -5,10 +7,12 @@ import { ZodError } from 'zod'
 import { env } from './env'
 import { ResourceNotFoundError } from './use-cases/errors/resource-not-found-error'
 import { globalErrorHandler } from './utils/global-error-handles'
+import { addressRoutes } from './http/controllers/address/routes'
 
 export const app = fastify()
 
 app.register(personRoutes)
 app.register(userRoutes)
+app.register(addressRoutes)
 
 app.setErrorHandler(globalErrorHandler)
